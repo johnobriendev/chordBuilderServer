ceadbf29cc0ca8faf9f9311c23a6f6fe3e3c7d14
John <140966972+johnobriendev@users.noreply.github.com>
2025-06-08 12:06:57 -0400

    fix: fix authAudience

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,9 @@
 import { auth } from 'express-oauth2-jwt-bearer';
 
+console.log('AUTH0_DOMAIN:', process.env.AUTH0_DOMAIN);
+console.log('AUTH0_AUDIENCE:', process.env.AUTH0_AUDIENCE);
+console.log('Constructed issuerBaseURL:', `https://${process.env.AUTH0_DOMAIN}`);
+
 // Create the JWT validation middleware - that's it!
 export const requireAuth = auth({
   audience: process.env.AUTH0_AUDIENCE,
