02a6025f3ad5efb88045b96db91af8ca43ae4ee0
John <140966972+johnobriendev@users.noreply.github.com>
2025-06-08 18:13:39 -0400

    use variable for cors

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://chordbuilder.netlify.app/', 'https://your-custom-domain.com'] 
+    ? process.env.FRONTEND_URL
     : 'http://localhost:5173',
   credentials: true
 }));
